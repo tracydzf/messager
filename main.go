371abfc6e113ae2e4359911af387b6637d165e46
tracydzf <934333109@qq.com>
2023-11-19 16:10:52 +0800

    发送到飞书中

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"golang.org/x/sync/errgroup"
+	_ "golang.org/x/sync/errgroup"
 	"log"
-	"messenger/global"
+	"messager/global"
+	"messager/middleware"
+	"messager/send"
 )
 
 func main() {
@@ -21,10 +25,24 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	gin.SetMode(gin.ReleaseMode)
-
+	gin.SetMode(gin.DebugMode)
 	r := gin.Default()
+	v1 := r.Group("/v1").Use(middleware.Auth(authConf), middleware.Error2Resp())
+	{
+		v1.POST("/message", send.PushMessage)
+		v1.POST("/uid/getbyphone", send.GetUIDByPhone)
 
+		v1.POST("/senders", global.PushRemoteConf)
+		v1.PUT("/senders", global.PushRemoteConf)
+		v1.DELETE("/senders", global.PushRemoteConf)
+
+	}
+
+	eg := &errgroup.Group{}
+	eg.Go(send.Start)
+	eg.Go(func() error {
+		return r.Run(fmt.Sprintf("%s:%s", appConf["ip"], appConf["port"]))
+	})
 	log.Println("start successfully...")
 	if err := eg.Wait(); err != nil {
 		log.Println(err)
